bca7dc1bb164f203b7f903a0e26710e6ab1f0f8c
El Hamed, Sidi <Sidi.El-Hamed@adesso.de>
2024-05-27 10:24:20 +0200

    Configure Swagger UI for Auth 2.0 & added UserController

--- security-service/src/main/java/org/pr1/securityservice/controllers/JWTController.java
+++ security-service/src/main/java/org/pr1/securityservice/controllers/JWTController.java
@@ -7,10 +7,6 @@ import org.pr1.securityservice.DTOs.RefreshTokenRequestDTO;
 import org.pr1.securityservice.services.JWTService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,11 +34,5 @@ public class JWTController {
             return new ResponseEntity<>(login, HttpStatus.UNAUTHORIZED);
         }
         return ResponseEntity.ok(login);
-    }
-    @GetMapping("/test")
-    @PreAuthorize("hasAnyAuthority('SCOPE_MANAGER')")
-    public String getTestDAta(){
-        System.out.println(SecurityContextHolder.getContext().getAuthentication());
-        return "User is successfully logged in";
     }
 }
